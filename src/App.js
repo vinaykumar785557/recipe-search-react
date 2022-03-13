7a52e4a94073d6dacd4f4d6dd1ebe71c77b01f63
vinaykumar785557 <c0785557@mylambton.ca>
2022-03-12 21:48:53 -0500

    api fetch is done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+	const [recipe, setRecipes] = useState([]);
+	const [query, setQuery] = useState('');
+
+	const YOUR_APP_ID = 'e091eb96';
+	const YOUR_APP_KEY = 'b4d1a4ceaef3e0223150957bc67d8a07';
+	const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=alcohol-free`;
+
+	const getRecipeInfo = async () => {
+		let result = await axios.get(url);
+
+		setRecipes(result.data.hits);
+		console.log(recipe);
+	};
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+		getRecipeInfo();
+	};
+
 	return (
 		<div className='app'>
 			<h1>
 				<u>Food Recipe Search</u>
 			</h1>
 
-			<div className='app__searchForm'>
+			<form className='app__searchForm' onSubmit={submitHandler}>
 				<input
+					value={query}
+					onChange={(e) => setQuery(e.target.value)}
 					type='text'
 					className='app__input'
 					placeholder='type something'
@@ -19,7 +42,7 @@ function App() {
 				</select>
 
 				<input type='submit' value='Get Recipe' className='app__submit'></input>
-			</div>
+			</form>
 		</div>
 	);
 }
