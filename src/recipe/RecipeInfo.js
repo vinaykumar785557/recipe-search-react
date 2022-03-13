d223dd26822725aa43a91efb30ad78104a479824
vinaykumar785557 <c0785557@mylambton.ca>
2022-03-12 22:56:41 -0500

    added regex to show different image formats

--- src/recipe/RecipeInfo.js
+++ src/recipe/RecipeInfo.js
@@ -3,14 +3,17 @@ import React from 'react';
 
 function RecipeInfo({ recipe }) {
 	return (
-		<div className={classes.recipeInfo}>
-			<img
-				className={classes.recipeInfo__image}
-				src={recipe['recipe']['image']}
-				alt={recipe['recipe']['label']}
-			/>
-			<p className={classes.recipeInfo__name}>{recipe['recipe']['label']} </p>
-		</div>
+		recipe['recipe']['image'].match(/\.(jpeg|jpg|gif|png)$/) != null && (
+			<div className={classes.recipeInfo}>
+				<img
+					className={classes.recipeInfo__image}
+					src={recipe['recipe']['image']}
+					alt={recipe['recipe']['label']}
+					onClick={() => window.open(recipe['recipe']['url'])}
+				/>
+				<p className={classes.recipeInfo__name}>{recipe['recipe']['label']} </p>
+			</div>
+		)
 	);
 }
 
