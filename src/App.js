b603a5942b8545cf78fb572e83ec41df9e354d93
vinaykumar785557 <c0785557@mylambton.ca>
2022-03-12 21:07:44 -0500

    search component created and styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Recipe Search
-    </div>
-  );
+	return (
+		<div className='app'>
+			<h1>
+				<u>Food Recipe Search</u>
+			</h1>
+
+			<div className='app__searchForm'>
+				<input
+					type='text'
+					className='app__input'
+					placeholder='type something'
+					autoComplete='Off'></input>
+
+				<select className='app__healthLabels'>
+					<option value='vegan'>vegan</option>
+				</select>
+
+				<input type='submit' value='Get Recipe' className='app__submit'></input>
+			</div>
+		</div>
+	);
 }
 
 export default App;
